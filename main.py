b76f6d3821e6b6030ae7e3ab509b174c4c0bae80
Foster <jjf@liverpool.ac.uk>
2025-03-05 11:19:31 +0000

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from config import API_URL, API_TOKEN
 from canvasapi import Canvas
 
 def read_csv(fname):
@@ -21,7 +20,7 @@ def read_csv(fname):
     except Exception as e:
         raise ValueError(f"Error reading the CSV file: {e}")
 
-def create_canvas_session(API_URL,API_TOKEN):
+def create_canvas_session(API_URL=None,API_TOKEN=None):
     if not API_URL or not API_TOKEN:    
         API_URL = input("Please input your canvas url, e.g 'https://canvas.liverpool.ac.uk': ")
         API_TOKEN = input("Please input your API token: ") 
@@ -32,18 +31,34 @@ def get_submissions(canvas, course_sis_id, assignment_id):
     submissions = [x for x in canvas.get_course(course_sis_id, use_sis_id=True).get_assignment(assignment_id).get_submissions(include=['submission_comments'])]
     return submissions
 
+def check_graded_and_posted(submission):
+    return submission.workflow_state =="graded" and submission.posted_at is not None
 
-
-if __name__=="__main__":
+def main():
     fname = input("Please enter filename, e.g 'assignments.csv': ")
     data = read_csv(fname)
-    canvas = create_canvas_session(API_URL,API_TOKEN) 
+    canvas = create_canvas_session() 
     print(canvas.get_current_user())
     data = data.sample(1)
+    message = "AUTOMATED MESSAGE: "
+    message += input("Please input global comment: ")
     for i,row in data.iterrows():
         print("course: ", row.course)
         submissions = get_submissions(canvas,row.course,row.assignment_id)
-        print(len(submissions))
+        
+
+        for submission in submissions:
+            if check_graded_and_posted(submission):
+                comment_string = " ".join([x["comment"]for x in submission.submission_comments]) 
+                if "AUTOMATED MESSAGE" not in comment_string: 
+                
+                    submission.edit(comment={"text_comment": message})
+
+
+
+if __name__=="__main__":
+    from config import API_URL,API_TOKEN
+    main()
 
 
 
